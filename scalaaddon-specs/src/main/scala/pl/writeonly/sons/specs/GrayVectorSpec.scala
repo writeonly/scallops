c9fb574f15cc4f0da35a1f3fa95a649743385d9a
Kamil Adam Żabiński <kamil.adam.zabinski@gmail.com>
2018-03-17 21:12:42 +0100

    refactoring

--- scalaaddon-specs/src/main/scala/pl/writeonly/sons/specs/GrayVectorSpec.scala
+++ scalaaddon-specs/src/main/scala/pl/writeonly/sons/specs/GrayVectorSpec.scala
@@ -1,7 +1,7 @@
 package pl.writeonly.sons.specs
 
-import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{Matchers, PropSpec}
 
 abstract class GrayVectorSpec
     extends PropSpec
