3bb892843c9cd2e674ef4c6b5e3ea7e9e46e5ed7
Kamil Adam Żabiński <kamil.adam.zabinski@gmail.com>
2018-04-01 21:48:14 +0200

    format

--- scalaaddon-utils/src/main/scala/pl/writeonly/sons/utils/future/FutureGetOrFailed.scala
+++ scalaaddon-utils/src/main/scala/pl/writeonly/sons/utils/future/FutureGetOrFailed.scala
@@ -6,14 +6,16 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 object FutureGetOrFailed {
-  def optFuture[A](value: Option[Future[A]])(
-      implicit ec: ExecutionContext): Future[A] = value match {
+  def optFuture[A](
+    value: Option[Future[A]]
+  )(implicit ec: ExecutionContext): Future[A] = value match {
     case Some(f: Future[A]) => f
     case None               => Future.failed(new IllegalStateException())
   }
 
-  def eitherFuture[A, B](value: Either[A, Future[B]])(
-      implicit ec: ExecutionContext): Future[B] =
+  def eitherFuture[A, B](
+    value: Either[A, Future[B]]
+  )(implicit ec: ExecutionContext): Future[B] =
     value match {
       case Right(f: Future[B]) => f
       case Left(f: A)          => Future.failed(toThrowable(f))
@@ -24,14 +26,16 @@ object FutureGetOrFailed {
     case _            => new IllegalStateException("" + a)
   }
 
-  def tryFuture[A](value: Try[Future[A]])(
-      implicit ec: ExecutionContext): Future[A] = value match {
+  def tryFuture[A](
+    value: Try[Future[A]]
+  )(implicit ec: ExecutionContext): Future[A] = value match {
     case Success(f: Future[A]) => f
     case Failure(f: Throwable) => Future.failed(f)
   }
 
-  def orFuture[A, B](value: Future[A] Or B)(
-      implicit ec: ExecutionContext): Future[A] = value match {
+  def orFuture[A, B](
+    value: Future[A] Or B
+  )(implicit ec: ExecutionContext): Future[A] = value match {
     case Good(f: Future[A]) => f
     case Bad(f: B)          => Future.failed(toThrowable(f))
   }
@@ -39,22 +43,26 @@ object FutureGetOrFailed {
   private def identity[A](a: A) = a
 
   implicit class FutureOpt[A](opt: Option[Future[A]])(
-      implicit ec: ExecutionContext) {
+    implicit ec: ExecutionContext
+  ) {
     def optFuture: Future[A] = FutureGetOrFailed.optFuture(opt)(ec)
   }
 
   implicit class FutureEither[A, B](value: Either[A, Future[B]])(
-      implicit ec: ExecutionContext) {
+    implicit ec: ExecutionContext
+  ) {
     def eitherFuture: Future[B] = FutureGetOrFailed.eitherFuture(value)(ec)
   }
 
   implicit class FutureTry[A](value: Try[Future[A]])(
-      implicit ec: ExecutionContext) {
+    implicit ec: ExecutionContext
+  ) {
     def tryFuture: Future[A] = FutureGetOrFailed.tryFuture(value)(ec)
   }
 
   implicit class FutureOr[A, B](value: Future[A] Or B)(
-      implicit ec: ExecutionContext) {
+    implicit ec: ExecutionContext
+  ) {
     def orFuture: Future[A] = FutureGetOrFailed.orFuture(value)(ec)
   }
 
--- scalaaddon-utils/src/main/scala/pl/writeonly/sons/utils/future/FutureSuccesful.scala
+++ scalaaddon-utils/src/main/scala/pl/writeonly/sons/utils/future/FutureSuccesful.scala
@@ -7,37 +7,42 @@ import scala.util.{Failure, Success, Try}
 
 object FutureSuccesful {
 
-  def opt[A](value: Future[A])(
-      implicit ec: ExecutionContext): Future[Option[A]] =
+  def opt[A](
+    value: Future[A]
+  )(implicit ec: ExecutionContext): Future[Option[A]] =
     //    value.transform({
     //      case Success(s) => Success(Option(s))
     //      case Failure(_) => Success(None)
     //    })
     transform(value, (s: A) => Option(s), { case _ => None })
 
-  private def transform[S, T](value: Future[T],
-                              s: T => S,
-                              pf: PartialFunction[Throwable, S])(
-      implicit executor: ExecutionContext): Future[S] =
+  private def transform[S, T](
+    value: Future[T],
+    s: T => S,
+    pf: PartialFunction[Throwable, S]
+  )(implicit executor: ExecutionContext): Future[S] =
     value
       .transform(s, e => e)
       .recover(pf)
 
-  def either[A](value: Future[A])(
-      implicit ec: ExecutionContext): Future[Either[Throwable, A]] =
+  def either[A](
+    value: Future[A]
+  )(implicit ec: ExecutionContext): Future[Either[Throwable, A]] =
     //    value.transform({
     //      case Success(s) => Success(Right(s))
     //      case Failure(t) => Success(Left(t))
     //    })
     transform(value, (s: A) => Right(s), { case t => Left(t) })
 
-  def tryWith[A](value: Future[A])(
-      implicit ec: ExecutionContext): Future[Try[A]] =
+  def tryWith[A](
+    value: Future[A]
+  )(implicit ec: ExecutionContext): Future[Try[A]] =
     //    value.transformWith(Future.successful)
     transform(value, (s: A) => Success(s), { case t => Failure(t) })
 
-  def or[A](value: Future[A])(
-      implicit ec: ExecutionContext): Future[A Or Throwable] =
+  def or[A](
+    value: Future[A]
+  )(implicit ec: ExecutionContext): Future[A Or Throwable] =
     //    value.transform({
     //      case Success(s) => Success(Good(s))
     //      case Failure(t) => Success(Bad(t))
@@ -45,7 +50,8 @@ object FutureSuccesful {
     transform(value, (s: A) => Good(s), { case t => Bad(t) })
 
   implicit class FutureSuccessful[A](value: Future[A])(
-      implicit ec: ExecutionContext) {
+    implicit ec: ExecutionContext
+  ) {
     def opt: Future[Option[A]] = FutureSuccesful.opt(value)(ec)
 
     def either: Future[Either[Throwable, A]] = FutureSuccesful.either(value)(ec)
