6383d4b064591b76994105f41268a807a423e593
Kamil Zabinski <kamil.zabinski@relayr.io>
2018-05-02 15:26:09 +0200

    add test

--- scala-for-future/src/test/scala/pl/writeonly/addons/future/scalaz/HydraFutureSpec.scala
+++ scala-for-future/src/test/scala/pl/writeonly/addons/future/scalaz/HydraFutureSpec.scala
@@ -1,5 +1,6 @@
 package pl.writeonly.addons.future.scalaz
 
+import org.scalatest.EitherValues
 import pl.writeonly.addons.future.RemoteService
 import pl.writeonly.addons.future.RemoteService.{CaseException, FutureResult}
 import pl.writeonly.addons.future.scalaz.HydraFuture._
@@ -8,7 +9,7 @@ import scalaz.{-\/, \/, \/-}
 
 import scala.concurrent.Future
 
-class HydraFutureSpec extends WhiteFutureSpec {
+class HydraFutureSpec extends WhiteFutureSpec with EitherValues {
   describe("A Hydra ") {
     describe("for Right with successful") {
       val v: String \/ FutureResult = \/-[FutureResult](Future.successful(1))
@@ -31,6 +32,33 @@ class HydraFutureSpec extends WhiteFutureSpec {
           i <- v.getOrFailed.transRecover
         } yield {
           i shouldBe \/-(1)
+        }
+      }
+    }
+    describe("for Left") {
+      val v: String \/ FutureResult = -\/(CaseException().message)
+      it("inSideOut") {
+        for {
+          i <- v.inSideOut
+        } yield {
+          i shouldBe -\/(CaseException().message)
+        }
+      }
+      it("getOrFailed") {
+        recoverToSucceededIf[IllegalStateException] {
+          for {
+            i <- v.getOrFailed
+          } yield {
+            i shouldBe 1
+          }
+
+        }
+      }
+      it("getOrFailed and transRecover") {
+        for {
+          i <- v.getOrFailed.transRecover
+        } yield {
+          i.toEither.left.value shouldBe a[IllegalStateException]
         }
       }
     }
