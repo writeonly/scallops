15aea3f2dd33206c241d64a9d9f53d0dc1e21c00
Kamil Adam Żabiński <kamil.adam.zabinski@gmail.com>
2018-05-13 23:08:53 +0200

    add ScallopsLogging

--- scala-logging/src/main/scala/pl/writeonly/scala/logging/ScallopsLogging.scala
+++ scala-logging/src/main/scala/pl/writeonly/scala/logging/ScallopsLogging.scala
@@ -1,15 +1,30 @@
 package pl.writeonly.scala.logging
 
-import akka.actor.{ActorRef, ActorSystem, Props}
-import akka.event.Logging
+import akka.actor.{ActorRef, ActorSystem, ExtendedActorSystem, Props}
+import akka.event._
 
 trait ScallopsLogging {
   protected def actorSystem: ActorSystem
 
-  @volatile private lazy val l = Logging.getLogger(actorSystem, this)
+  @volatile private lazy val l: DiagnosticLoggingAdapter =
+    ScallopsLogging.getLogger(actorSystem, this)
 
   @volatile lazy val actorRef: ActorRef =
     actorSystem.actorOf(Props(classOf[LoggingActor], l))
 
   @volatile lazy val logger = new LoggingWrapper(l, actorRef)
+
+}
+
+object ScallopsLogging {
+  def getLogger(system: ActorSystem,
+                logSource: AnyRef): DiagnosticLoggingAdapter = {
+    val (str, clazz) = LogSource.fromAnyRef(logSource, system)
+    new BusLogging(
+      system.eventStream,
+      str,
+      clazz,
+      system.asInstanceOf[ExtendedActorSystem].logFilter
+    ) with DiagnosticLoggingAdapter
+  }
 }
---
+++ scala-logging/src/test/scala/pl/writeonly/scala/logging/ScallopsLoggingScalarSpec.scala
@@ -0,0 +1,38 @@
+package pl.writeonly.scala.logging
+
+import akka.actor.ActorSystem
+import akka.event.Logging.MDC
+import pl.writeonly.scalaops.specs.GrayScalarSpec
+
+class ScallopsLoggingScalarSpec extends GrayScalarSpec {
+  it should "log message use logger" in {
+    implicit val mdc: MDC = Map("key" -> "value")
+
+    val logging = new ScallopsLogging {
+      override implicit protected val actorSystem: ActorSystem = ActorSystem()
+
+      def logMessage(): Unit = {
+        logger.error(new IllegalStateException(), "it is error !")
+        logger.error(
+          new IllegalStateException(),
+          "it is error with {} !",
+          "arg"
+        )
+        logger.error("it is error !")
+        logger.error("it is error with {} !", "arg")
+        logger.warning("it is warning !")
+        logger.warning("it is warning with {} !", "arg")
+        logger.info("it is info !")
+        logger.info("it is info with {} !", "arg")
+        logger.debug("it is debug !")
+        logger.debug("it is debug with {} !", "arg")
+      }
+    }
+    logging.logMessage()
+
+    logging.logger.isErrorEnabled shouldBe true
+    logging.logger.isWarningEnabled shouldBe true
+    logging.logger.isInfoEnabled shouldBe true
+    logging.logger.isDebugEnabled shouldBe false
+  }
+}
